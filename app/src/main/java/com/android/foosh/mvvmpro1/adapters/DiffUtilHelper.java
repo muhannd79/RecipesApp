09206d25c7d08128c189cc34a6a599ad0e774247
muhannd79 <muhannddh@gmail.com>
2020-02-10 10:43:55 -0600

    RecyclerView with Multiple View Types part

---
+++ app/src/main/java/com/android/foosh/mvvmpro1/adapters/DiffUtilHelper.java
@@ -0,0 +1,51 @@
+package com.android.foosh.mvvmpro1.adapters;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import com.android.foosh.mvvmpro1.models.Recipe;
+
+import java.util.List;
+
+class DiffUtilHelper extends DiffUtil.Callback {
+
+    List<Recipe> oldRecipes;
+    List<Recipe> newRecipes;
+
+    public DiffUtilHelper(List<Recipe> oldRecipes, List<Recipe> newRecipes) {
+
+        this.oldRecipes = oldRecipes;
+        this.newRecipes =newRecipes;
+
+    }
+
+    @Override
+    public int getOldListSize() {
+        if(oldRecipes ==null){
+            return 0;
+        } else {
+            return oldRecipes.size();
+        }
+    }
+
+    @Override
+    public int getNewListSize() {
+
+        if(newRecipes ==null){
+            return 0;
+        } else {
+            return newRecipes.size();
+        }
+
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldRecipes.get(newItemPosition) == newRecipes.get(newItemPosition);
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+
+        return oldRecipes.get(newItemPosition) == newRecipes.get(newItemPosition);
+    }
+}
